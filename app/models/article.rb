0853fced400582dc15b1120cff22a6b7b9e27a7f
Guillermo Segalerba <guillermosegalerba@Guillermos-MacBook-Pro.local>
2014-07-31 16:49:55 -0300

    Adding annotate gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  text       :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Article < ActiveRecord::Base
   has_many :comments , dependent: :destroy
   validates :title, presence: true,
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  commenter  :string(255)
+#  body       :text
+#  article_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
